bb126d01768d1d1acb84b7958c35b6691a6cd9a1
Mal <malayappa@gmail.com>
2025-06-09 11:50:04 +0800

    update

--- Project2/ecommerce/src/main/java/com/example/ecommerce/controllers/UserController.java
+++ Project2/ecommerce/src/main/java/com/example/ecommerce/controllers/UserController.java
@@ -1,11 +1,13 @@
 package com.example.ecommerce.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ecommerce.services.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.services.UserService;
   
 @RestController
 @CrossOrigin("*")
--- Project2/ecommerce/src/main/java/com/example/ecommerce/services/UserService.java
+++ Project2/ecommerce/src/main/java/com/example/ecommerce/services/UserService.java
@@ -1,21 +1,21 @@
-package com.services;
+package com.example.ecommerce.services;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.ecommerce.models.User;
+import com.example.ecommerce.repository.UserRep;
 
 @Service
 public class UserService {
-    public List<User> getList() {    
-        User userOne = new User(1L, "RAJA", "raja@example.com", "9998987", "TRICHY");
-        User userTwo = new User(2L, "KUMAR", "kumar@example.com","9998988", "TRICHY");
-
+    @Autowired
+    UserRep userRep;
+    public List<User> getList() {
         List<User> users = new ArrayList<>();
-        users.add(userOne);
-        users.add(userTwo);
+        // Simulating a database fetch
 
         return users;
     }
